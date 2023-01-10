74dbb5ef7058b5480e04374a0ad5185fc7e3cc0f
nastib <nastib@outlook.com>
2023-01-10 21:04:53 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/vue.svg" style="height: 100px; width: 100px;">
+    <readJSON/>
+  </div>
+</template>
+<script setup lang="ts">
+import ReadJSON from './components/ReadJSON.vue'
+</script>
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ReadJSON.vue
@@ -0,0 +1,30 @@
+The template then loads the JSON in a couple of diffferent ways-->
+
+<template>
+  <span>
+    <p>JSON DATA: </p>
+    <ul>
+      <li v-for="product in jsonData" :key="product.id" style="list-style-type: none;">
+     <b>Name:</b> {{ product.title }}, <br> <p><b>Price: $</b> {{ product.price }}</p>, 
+    <p> <b>Photo</b></p> <img :src="product.image" alt="Photo" style="  height: 100px;
+     width: 100px;">
+      </li>
+    </ul>
+  </span>
+</template>
+
+<script setup lang="ts">import { onMounted, ref } from 'vue';
+const jsonData = ref()
+onMounted(async () => {
+  const response = await fetch('https://fakestoreapi.com/products?limit=5')
+  jsonData.value = await response.json();
+   
+  }
+);
+</script>
+<style scoped>
+span {
+  font-size: 1.2em;
+  text-align: center;
+}
+</style>
